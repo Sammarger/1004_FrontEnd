19454460c30ed3aeb6187691b832b77b93ea4960
Samuel Margerison <SamR.Margerison@gmail.com>
2024-04-26 19:46:46 +0100

    Supabase connection

--- backend.js
+++ backend.js
@@ -1,18 +1,22 @@
 
+
+import { createClient } from '@supabase/supabase-js'
+
+const supabaseUrl = 'https://ztncxmihptraxhebbwqd.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp0bmN4bWlocHRyYXhoZWJid3FkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTQwNjI3NzAsImV4cCI6MjAyOTYzODc3MH0.DYV7wVkw2fUq2sVpmzTf4X2GPl1V0U6VSkJgETgWCm8'
+const supabase = createClient(supabaseUrl, supabaseKey)
+
 const form = document.querySelector("form");
 const name = document.getElementById("name");
 const license = document.getElementById("license");
 const para = document.querySelector("p");
 
+async function fetchData() {
+    const {data, error} = await supabase.from('Vehicle').select();
+    console.log('Fetched data:', data)
+}
+
 form.addEventListener("submit", (e) => {
     e.preventDefault();
-    if(name.value != "") {
-        para.textContent =`${name}`;
-    } 
-    else if (license.value != ""){
-        para.textContent = `${license}`;
-    }
-    else {
-    para.textContent = "You need to enter something into a box.";
-    }
+    fetchData();
 });
