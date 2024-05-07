51b990678206f8c1c63b2b37e4239bb077de6d20
Samuel Margerison <SamR.Margerison@gmail.com>
2024-05-07 15:05:31 +0100

    add js files

---
+++ add-a-vehicle-backend.js
@@ -0,0 +1,113 @@
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
+
+const supabaseUrl = 'https://ztncxmihptraxhebbwqd.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp0bmN4bWlocHRyYXhoZWJid3FkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTQwNjI3NzAsImV4cCI6MjAyOTYzODc3MH0.DYV7wVkw2fUq2sVpmzTf4X2GPl1V0U6VSkJgETgWCm8';
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+// Vehicle Form Inputs
+const vehicleForm = document.getElementById("vehicle-form");
+const rego = document.getElementById("rego");
+const make = document.getElementById("make");
+const model = document.getElementById("model");
+const colour = document.getElementById("colour");
+const owner = document.getElementById("owner");
+
+// People Form Inputs
+const peopleForm = document.getElementById("people-form");
+const personID = document.getElementById("personid");
+const name = document.getElementById("name");
+const address = document.getElementById("address");
+const dob = document.getElementById("dob");
+const license = document.getElementById("license");
+const expire = document.getElementById("expire");
+
+// Other
+var message = document.querySelector("p");
+
+
+
+function toString(item) {
+    return JSON.stringify(item).replace(/"/g, '') //Converts table item to a string
+}
+
+// ------------ VEHICLE FORM FUNCTIONS -------------- //
+
+async function findOwner(ownerName) {
+    const {data, error} = await supabase.from('People').select(''); // Retreives whole people table from supabase
+
+    for(const person of data) {
+        let personName = toString(person["Name"]);
+        if(personName === ownerName) // If the name matches the user input, return ownerID 
+        {
+            return toString(person["PersonID"])
+        }
+    }
+    return 0;
+}
+function validVehicleFormInputs(){ // Makes sure there are no null values entered
+    if(rego === "" || make === "" || model === "" || colour === ""){ 
+        return false;
+    } else {
+        return true;
+    }
+
+}
+
+async function addVehicle(ownerID){ // Add vehicle to table
+    const { error } = await supabase.from('Vehicle')
+    .insert({ VehicleID: rego.value, Make: make.value, Model: model.value, Colour: colour.value, OwnerID: ownerID})
+}
+
+
+vehicleForm.addEventListener("submit", async (e) => { // Event must be async as function calls for an sql table
+    e.preventDefault(); // Prevents form from loading a new page
+    var ownerID;
+    if(owner.value != ""){  // If value entered is not nothing attempt to find person
+        ownerID = await findOwner(owner.value);
+    } else {
+        ownerID = -1 // If null value entered return error
+    }
+    if(ownerID === 0) {
+        peopleForm.style.display = "block"; 
+        message.textContent = "";
+    }
+    else if(ownerID === -1){
+        peopleForm.style.display = "none"; // Removes people form if on screen
+        message.textContent = "Error"
+    } else {
+        peopleForm.style.display = "none"; // Removes people form if on screen
+        if(validVehicleFormInputs())
+        {
+            await addVehicle(ownerID);
+            message.textContent = `Vehicle Added Successfully`;
+        } else {
+            message.textContent = "Error";
+        }
+    }
+});
+
+// ------------ PEOPLE FORM FUNCTIONS -------------- //
+
+function validPeopleFormInputs(){
+    if(personID.value === "" || name.value === "" || address.value === "" || dob.value === "" || license.value === "" || expire.value === "" ){
+        return false;
+    } else {
+        return true;
+    }
+}
+async function addPerson(){ // Add vehicle to table
+    const { error } = await supabase.from('Vehicle')
+    .insert({ PersonID: personID.value, Name: name.value, Address: address.value, DOB: dob.value, LicenseNumber: license.value, ExpiryDate: expire.value})
+}
+
+peopleForm.addEventListener("submit", async(e) => {
+    e.preventDefault();
+
+    if(validPeopleFormInputs()){
+        await addPerson();
+        message.textContent = "Success";
+    } else {
+        message.textContent = "Error";
+    }
+});
+
---
+++ vehicle-backend.js
@@ -0,0 +1,89 @@
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
+
+const supabaseUrl = 'https://ztncxmihptraxhebbwqd.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp0bmN4bWlocHRyYXhoZWJid3FkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTQwNjI3NzAsImV4cCI6MjAyOTYzODc3MH0.DYV7wVkw2fUq2sVpmzTf4X2GPl1V0U6VSkJgETgWCm8';
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+const form = document.querySelector("form");
+const rego = document.getElementById("rego");
+var results = document.getElementById("results");
+var message = document.querySelector("p");
+var tableOutput;
+
+
+function matchingItems(item) {
+    const stringLine = "<div>Registration Number: " + item.vehicleID + ", Make: " + item.make + ", Model: " + item.model +
+    ", Colour: "+ item.colour + ", Vehicle Owner: " + item.owner + "</div>";
+
+    if(rego.value === item.vehicleID) {
+        tableOutput = tableOutput + stringLine;
+    }
+}
+
+function getOwnerName(ownerID, peopleData) { // Goes through each person in the person table, if ownerID matches, add vehicle owner
+    for(const person of peopleData) {
+        let personID = JSON.stringify(person["PersonID"]).replace(/"/g, '');
+
+        if(personID === ownerID) {
+            let personName = JSON.stringify(person["Name"]).replace(/"/g, '');
+            return personName;
+        }
+    }
+}
+
+function createVehicle(item, peopleData){ 
+
+    const vehicle = {};
+    vehicle.vehicleID = (JSON.stringify(item["VehicleID"])).replace(/"/g, '');
+    vehicle.make = (JSON.stringify(item["Make"])).replace(/"/g, '');
+    vehicle.model = (JSON.stringify(item["Model"])).replace(/"/g, '');
+    vehicle.colour = (JSON.stringify(item["Colour"])).replace(/"/g, '');
+
+    let ownerID = JSON.stringify(item["OwnerID"]).replace(/"/g, '')
+
+    if(ownerID != "") { 
+        vehicle.owner = getOwnerName(ownerID, peopleData);
+    } else {
+        vehicle.owner = 'NULL';
+    } 
+    return vehicle;
+}
+
+async function fetchData(table) { // Fetches the whole tanle from supabase
+    const {data, error} = await supabase.from(table).select('');
+
+    return data;
+}
+
+function searchResults() {
+    if(rego.value === "") {
+        message.textContent = "Error";
+    } else {
+        if(tableOutput === "") {
+            message.textContent = "No result found";
+        } else {
+            message.textContent = "Search successful";
+            results.innerHTML = tableOutput;
+
+        }
+    }
+}
+
+form.addEventListener("submit", async (e) => { // Event must be async as function calls for an sql table
+    e.preventDefault(); // Prevents form from loading a new page
+
+    tableOutput = ""; // Text that is to be output will be entered here,
+                      // such that if there is an error, output will be blank,
+                      // instead of the previous search results
+    const vehicleData = await fetchData('Vehicle'); 
+
+    const peopleData = await fetchData('People'); // People table needed to find owner name from owner id
+
+    for(const item of vehicleData){ // For every vehicle in vehicleData, a vehicle object is created, and the owner is identified
+
+        var vehicle = createVehicle(item, peopleData);
+        matchingItems(vehicle); // Checks if there is a vehicle matching the user input
+    }
+
+    searchResults(); // Changes the output depending on the results
+});
