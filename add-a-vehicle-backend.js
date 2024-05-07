6cbd71e8f139347c1ab6015b9a92cf862c5c1b41
Samuel Margerison <SamR.Margerison@gmail.com>
2024-05-07 15:54:43 +0100

    test

--- add-a-vehicle-backend.js
+++ add-a-vehicle-backend.js
@@ -1,6 +1,4 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
-import { text } from 'stream/consumers';
-
 const supabaseUrl = 'https://ztncxmihptraxhebbwqd.supabase.co';
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp0bmN4bWlocHRyYXhoZWJid3FkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTQwNjI3NzAsImV4cCI6MjAyOTYzODc3MH0.DYV7wVkw2fUq2sVpmzTf4X2GPl1V0U6VSkJgETgWCm8';
 const supabase = createClient(supabaseUrl, supabaseKey);
